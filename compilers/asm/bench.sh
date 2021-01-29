71aea5884f01acfc17e4b4006bf1bbf438ce3907
Sylvain Gadrat <sgadrat@wontfix.it>
2021-01-29 01:01:09 +0100

    compilers: add ASM

--- compilers/asm/bench.sh
+++ compilers/asm/bench.sh
@@ -3,28 +3,17 @@
 set -e
 #set -x
 
-# Setup environment
-if [ -z "$VBCC" ]; then
-	export VBCC=~/workspace/nes/6502_compil/c/vbcc6502/vbcc6502_linux/vbcc
-	export PATH+=:$VBCC/bin
-fi
-script_dir="`dirname $0`"
-
 # Compil test's main
 ~/workspace/nes/cc65/bin/ca65 bench.s
 
 # Build variants
 variant() {
 	local variant_name="$1"
-	local options="$2"
-	vc $options unzip.c -S -o unzip_${variant_name}_built.s
-	"${script_dir}/asm_convert" unzip_${variant_name}_built.s
-	~/workspace/nes/cc65/bin/ca65 unzip_${variant_name}_built.s
+	~/workspace/nes/cc65/bin/ca65 unzip.s -o unzip_${variant_name}_built.o
 	~/workspace/nes/cc65/bin/ld65 -o bench_${variant_name}.6502 -C ld65.cfg --dbgfile bench_${variant_name}.dbg bench.o unzip_${variant_name}_built.o
 }
 
-variant "o3" "-O3"
-variant "o4" "-O4"
+variant "asm"
 
 #
 # Extract metrics
@@ -47,5 +36,4 @@ extract() {
 	echo "code size: $size"
 }
 
-extract "o3"
-extract "o4"
+extract "asm"
